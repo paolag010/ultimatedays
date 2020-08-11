3f79f5955c5da9d7d11948a29fb820bd1ec21d67
Paola G <paolag010@gmail.com>
2020-08-11 14:09:33 +0200

    Correction de l'erreur

--- src/main/java/ultimateDays/hello/App.java
+++ src/main/java/ultimateDays/hello/App.java
@@ -9,6 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! Avec ma modification" );
-        System.out.println( "value " +  getValue() );
+        System.out.println( "value : " +  getValue() );
+    }
+    
+    public static String getValue() {
+    	return "test";
     }
 }
